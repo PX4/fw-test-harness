11185faef0bd22ad0c7805ca2825dea4119892e0
Thomas Gubler <thomasgubler@gmail.com>
2015-05-03 08:56:34 +0200

    compile roll and yaw controller

--- Firmware_custom/ecl/ecl.h
+++ Firmware_custom/ecl/ecl.h
@@ -1,3 +1,7 @@
-// XXX provide interface for time
-#define ecl_absolute_time(_arg) 0 
-#define ecl_elapsed_time(_arg) 0
+#include <cstdint>
+
+// XXX just some hacks for now, provide interface for time later
+#define ecl_absolute_time() 0
+#define ecl_elapsed_time(_arg) *(_arg)
+typedef std::uint64_t    hrt_abstime;
+
