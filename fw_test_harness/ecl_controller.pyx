430fd0b73238397220c02b351fc2a276c78e1416
Thomas Gubler <thomasgubler@gmail.com>
2015-05-10 10:54:09 +0200

    build and link a cython ecl controller module (test code only)

--- fw_test_harness/ecl_controller.pyx
+++ fw_test_harness/ecl_controller.pyx
@@ -1,1 +1,4 @@
-print "hello world"
+cdef extern from "ecl_pitch_controller.h":
+    cdef cppclass ECL_PitchController:
+        ECL_PitchController()
+cdef  ECL_PitchController *pc = new ECL_PitchController()
